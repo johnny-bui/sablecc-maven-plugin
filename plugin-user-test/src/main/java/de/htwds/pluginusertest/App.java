a698faa817e2735161eceeced8184579db84a9d7
hbui <>
2013-01-29 18:30:57 +0100

    test Plugin OK

---
+++ plugin-user-test/src/main/java/de/htwds/pluginusertest/App.java
@@ -0,0 +1,17 @@
+package de.htwds.pluginusertest;
+
+import de.Generate;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		Generate g = new Generate();
+		g.hi();
+    }
+}
