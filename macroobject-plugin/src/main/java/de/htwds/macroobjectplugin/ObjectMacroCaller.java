063f1291218eb7dfb61688bf0a4048f7069b7a88
hbui <>
2013-01-31 16:23:59 +0100

    Kann Datein in richtige Ordner erzeugen

--- macroobject-plugin/src/main/java/de/htwds/macroobjectplugin/ObjectMacroCaller.java
+++ macroobject-plugin/src/main/java/de/htwds/macroobjectplugin/ObjectMacroCaller.java
@@ -4,15 +4,17 @@
  */
 package de.htwds.macroobjectplugin;
 
-import com.sun.media.sound.FFT;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.sablecc.objectmacro.launcher.ObjectMacro;
 
 /**
  * Says "Hi" to the user.
@@ -23,55 +25,194 @@ import org.apache.maven.plugins.annotations.Parameter;
 public class ObjectMacroCaller extends AbstractMojo {
 
 	/**
-	 * The greeting to display.
+	 * Output Type (Java, etc... depends on ObjectMacro)
 	 */
-	@Parameter(defaultValue = "Hello World!")
-	private String greeting;
 	@Parameter(defaultValue = "java")
-	private String outputType;
+	private String language;
 	@Parameter
-	private String outDir;
+	private String directory;
+	@Parameter(defaultValue = "template")
+	private String packagename;
+	/**
+	 * true => --generate-code, false => --no-code
+	 */
+	@Parameter(defaultValue = "true")
+	private boolean generateCode;
+	/**
+	 * true => strict false => lenient
+	 */
+	@Parameter(defaultValue = "true")
+	private boolean strict;
+	/**
+	 * informative => --informativ quite => --quiet verbose => --verbose
+	 */
+	@Parameter(defaultValue = "informative")
+	private String informative;
+	@Parameter
+	private List<Map> templates;
 	private final String fileSep = System.getProperty("file.separator");
 	private String baseDir = System.getProperty("project.basedir");
-
-	public void execute() throws MojoExecutionException {
-		getLog().info(">>>>>>>>>>>>>>>" + greeting + "<<<<<<<<<<<<<<<<<<<");
-		getLog().info(">>>>>>>>>>>>>>>" + outputType + "<<<<<<<<<<<<<<<<<<");
+	private static final String GEN_CODE = "--generate-code";
+	private static final String NO_CODE = "--no-code";
+	private static final String STRICT = "--strict";
+	private static final String LENIENT = "--lenient";
+	private static final String INFORMATIVE = "--informative";
+	private static final String VERBOSE = "--verbose";
+	private static final String QUIET = "--quiet";
+	
+	private static final Set<String> packageNameNoGo = new HashSet<String>();
+	static {// TODO add more 
+		packageNameNoGo.add("..");
+		packageNameNoGo.add("#");
+		packageNameNoGo.add("@");
+		packageNameNoGo.add("?");
+		packageNameNoGo.add("/");
+		packageNameNoGo.add("%");
+		packageNameNoGo.add(" ");
+	}
+	public void execute() throws MojoFailureException {
 		try {
 			constructOutDir();
-			String outputFile = outDir + "de" + fileSep + "Generate.java";
-			
-			getLog().info(outputFile);
-			File f = new File(outputFile);
-			if (!f.exists()) {
-				f.getParentFile().mkdirs();
-				f.createNewFile();
+			for (Map m : templates) {
+				String[] argv = parseArgument(m);
+				getLog().info("call ObjectMacro with argv:");
+				for (String s:argv){
+					getLog().info(s);
+				}
+				ObjectMacro.compile(argv);
+				/*try{
+					ObjectMacro.compile(argv);
+				}catch (Exception mex){
+					getLog().warn(mex.getMessage());
+					getLog().debug(mex);
+				}*/
 			}
-			FileWriter fw = new FileWriter(f,false);
-			fw.write("package de;\n");
-			fw.append("public class Generate{ public void hi(){System.out.println(\"hi\");}}\n");
-			fw.close();
-		} catch (IOException ex) {
-			throw new MojoExecutionException("Write to file error", ex);
+		}catch(RuntimeException ex){
+			throw new MojoFailureException("Compile template file error: " + ex.getMessage(), ex);
+		}catch (Exception ex) {
+			throw new MojoFailureException("Compile template file error: " + ex.getMessage(), ex);
 		}
 
+	}
 
+	private String[] parseArgument(Map m) {
+		List<String> arg = new ArrayList<String>();
+
+		// option "-t language"
+		String l = (String) m.get("language");
+		String localLanguage = (isOptionValid(l)) ? l.trim() : language;
+		arg.add("-t");
+		arg.add(localLanguage);
+		// option "-d directory" // TODO: check validation directory
+		String d = (String) m.get("directory");
+		String localDirectory = (isOptionValid(d)) ? d.trim() : directory;
+		arg.add("-d");
+		arg.add(localDirectory);
+		// option "-p packagesname"
+		String p = (String) m.get("packagename");
+		if (p!=null){
+			if (isPackageNameValue(p)){
+				String localPackagename = p.trim();
+				arg.add("-p");
+				arg.add(localPackagename);
+			}else{
+				throw new RuntimeException("package name not valid:" + p.trim());
+			}
+		}else{
+			if (isPackageNameValue(packagename)){
+				String localPackagename = packagename.trim();
+				arg.add("-p");
+				arg.add(localPackagename);
+			}else{
+				throw new RuntimeException("package name not valid:" + packagename.trim());
+			}
+		}
+		// option "--generate-code" or "--no-code"
+		String localGenerateCode = generateCode ? GEN_CODE : NO_CODE;
+		String g = (String) m.get("generateCode");
+		if (g != null) {
+			g = g.trim().toLowerCase();
+			localGenerateCode = (g.equals("true") || g.equals("generate-code")) ? GEN_CODE : NO_CODE;
+		}
+		arg.add(localGenerateCode);
+		// option "--strict" or "--lenient"
+		String localStrict = strict ? STRICT : LENIENT;
+		String s = (String) m.get("strict");
+		if (s != null) {
+			s = s.trim().toLowerCase();
+			localStrict = (s.equals("true") || s.equals("strict")) ? STRICT : LENIENT;
+		}
+		arg.add(localStrict);
+		// option "--quiet" or "--informative" or "--verbose"
+		String localInformative = INFORMATIVE;
+		String i = (String) m.get("informative");
+		if (i != null) {
+			i = i.trim().toLowerCase();
+			if (i.equals("quiet")) {
+				localInformative = QUIET;
+			} else if (i.equals("verbose")) {
+				localInformative = VERBOSE;
+			} else if (i.equals("informative")) {
+				localInformative = INFORMATIVE;
+			}
+		} else {
+			informative = informative.trim().toLowerCase();
+			if (informative.equals("quiet")) {
+				localInformative = QUIET;
+			} else if (informative.equals("informative")) {
+				localInformative = INFORMATIVE;
+			} else if (informative.equals("verbose")) {
+				localInformative = VERBOSE;
+			}
+		}
+		arg.add(localInformative);
+		// the template file
+		String file = (String) m.get("file");
+		//File f = new File(file);
+		arg.add(file);
+		// output
+		String[] argv = arg.toArray(new String[0]);
+		return argv;
 	}
 
 	private String constructOutDir() {
 		if (baseDir == null) {
 			baseDir = ".";
 		}
-		if (outDir == null) {
-			outDir = baseDir + fileSep
+		if (directory == null) {
+			directory = baseDir + fileSep
 					+ "target" + fileSep
 					+ "generated-sources" + fileSep
 					+ "objectmacro" + fileSep;
 		} else {
-			if (!outDir.endsWith(fileSep)) {
-				outDir += fileSep;
+			if (!directory.endsWith(fileSep)) {
+				directory += fileSep;
 			}
 		}
-		return outDir;
+		return directory;
+	}
+
+	boolean isOptionValid(String option){
+		if (option==null){
+			return false;
+		}
+		if (option.trim().length()==0){
+			return false;
+		}
+		return true;
+	}
+	boolean isPackageNameValue(String pn){
+		if (!isOptionValid(pn)){
+			return false;
+		}
+		if (pn.trim().startsWith(".")){
+			return false;
+		}
+		for (String n:packageNameNoGo){
+			if (pn.contains(n)){
+				return false;
+			}
+		}
+		return true;
 	}
 }
