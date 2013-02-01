342f64fbe1b6c0846612edc97eb60c8e90cad0c5
hbui <>
2013-02-01 18:29:40 +0100

    TODO: schreibe test wenn zeit habe

---
+++ objectmacro-plugin/src/test/java/de/htwds/objectmacroplugin/ObjectMacroCallerTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.htwds.objectmacroplugin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hbui
+ */
+public class ObjectMacroCallerTest {
+	
+	public ObjectMacroCallerTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testExecute() throws Exception {
+		// TODO write test here !!!!!!!
+	}
+}
