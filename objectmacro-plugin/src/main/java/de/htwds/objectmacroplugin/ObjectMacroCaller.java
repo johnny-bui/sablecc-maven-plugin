83547e1d65140d6059aa39de73bc17d5bf7c95e2
verylazyboy <phucluoi@gmail.com>
2013-02-02 21:20:31 +0100

    fine tuning

--- objectmacro-plugin/src/main/java/de/htwds/objectmacroplugin/ObjectMacroCaller.java
+++ objectmacro-plugin/src/main/java/de/htwds/objectmacroplugin/ObjectMacroCaller.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.htwds.objectmacroplugin;
 
 import java.util.ArrayList;
@@ -18,6 +14,7 @@ import org.sablecc.objectmacro.launcher.ObjectMacro;
 
 /**
  * Call ObjectMacro to generate Java file from ObjectMacro file.
+ *
  * @author Hong Phuc Bui
  * @version 1.0-SNAPSHOT
  *
@@ -35,30 +32,23 @@ public class ObjectMacroCaller extends AbstractMojo {
 	private String directory;
 	@Parameter(defaultValue = "template")
 	private String packagename;
-	
 	/**
 	 * true => --generate-code, false => --no-code.
 	 */
 	@Parameter(defaultValue = "true")
 	private boolean generateCode;
-	
 	/**
 	 * true => strict false => lenient.
 	 */
 	@Parameter(defaultValue = "true")
 	private boolean strict;
-	
 	/**
 	 * informative => --informativ quite => --quiet verbose => --verbose.
 	 */
 	@Parameter(defaultValue = "informative")
 	private String informative;
-
-	
 	@Parameter
 	private List<Map> templates;
-
-	
 	private final String fileSep = System.getProperty("file.separator");
 	private String baseDir = System.getProperty("project.basedir");
 	private static final String GEN_CODE = "--generate-code";
@@ -68,7 +58,6 @@ public class ObjectMacroCaller extends AbstractMojo {
 	private static final String INFORMATIVE = "--informative";
 	private static final String VERBOSE = "--verbose";
 	private static final String QUIET = "--quiet";
-	
 	private static final Set<String> packageNameNoGo = new HashSet<String>();
 	static {// TODO add more 
 		packageNameNoGo.add("..");
@@ -79,109 +68,122 @@ public class ObjectMacroCaller extends AbstractMojo {
 		packageNameNoGo.add("%");
 		packageNameNoGo.add(" ");
 	}
+
 	public void execute() throws MojoFailureException {
 		try {
 			constructOutDir();
 			for (Map m : templates) {
-				String[] argv = parseArgument(m);
-				getLog().info("call ObjectMacro with argv:");
-				for (String s:argv){
-					getLog().info(s);
+				List<String> argv = parseArgument(m);
+				if (argv != null) {
+					getLog().info("call ObjectMacro with argv:");
+					//for (String s : argv) {
+					//	getLog().info(s);
+					//}
+					getLog().info(argv.toString());
+					ObjectMacro.compile(argv.toArray(new String[0]));
 				}
-				ObjectMacro.compile(argv);
 				/*try{
-					ObjectMacro.compile(argv);
-				}catch (Exception mex){
-					getLog().warn(mex.getMessage());
-					getLog().debug(mex);
-				}*/
+				 ObjectMacro.compile(argv);
+				 }catch (Exception mex){
+				 getLog().warn(mex.getMessage());
+				 getLog().debug(mex);
+				 }*/
 			}
-		}catch(RuntimeException ex){
+		} catch (RuntimeException ex) {
 			throw new MojoFailureException("Compile template file error: " + ex.getMessage(), ex);
-		}catch (Exception ex) {
+		} catch (Exception ex) {
 			throw new MojoFailureException("Compile template file error: " + ex.getMessage(), ex);
 		}
 
 	}
 
-	private String[] parseArgument(Map m) {
+	private List<String> parseArgument(Map m) {
 		List<String> arg = new ArrayList<String>();
-
-		// option "-t language"
-		String l = (String) m.get("language");
-		String localLanguage = (isOptionValid(l)) ? l.trim() : language;
-		arg.add("-t");
-		arg.add(localLanguage);
-		// option "-d directory" // TODO: check validation directory
-		String d = (String) m.get("directory");
-		String localDirectory = (isOptionValid(d)) ? d.trim() : directory;
-		arg.add("-d");
-		arg.add(localDirectory);
-		// option "-p packagesname"
-		String p = (String) m.get("packagename");
-		if (p!=null){
-			if (isPackageNameValue(p)){
-				String localPackagename = p.trim();
-				arg.add("-p");
-				arg.add(localPackagename);
-			}else{
-				throw new RuntimeException("package name not valid:" + p.trim());
-			}
-		}else{
-			if (isPackageNameValue(packagename)){
-				String localPackagename = packagename.trim();
-				arg.add("-p");
-				arg.add(localPackagename);
-			}else{
-				throw new RuntimeException("package name not valid:" + packagename.trim());
-			}
-		}
-		// option "--generate-code" or "--no-code"
-		String localGenerateCode = generateCode ? GEN_CODE : NO_CODE;
-		String g = (String) m.get("generateCode");
-		if (g != null) {
-			g = g.trim().toLowerCase();
-			localGenerateCode = (g.equals("true") || g.equals("generate-code")) ? GEN_CODE : NO_CODE;
-		}
-		arg.add(localGenerateCode);
-		// option "--strict" or "--lenient"
-		String localStrict = strict ? STRICT : LENIENT;
-		String s = (String) m.get("strict");
-		if (s != null) {
-			s = s.trim().toLowerCase();
-			localStrict = (s.equals("true") || s.equals("strict")) ? STRICT : LENIENT;
-		}
-		arg.add(localStrict);
-		// option "--quiet" or "--informative" or "--verbose"
-		String localInformative = INFORMATIVE;
-		String i = (String) m.get("informative");
-		if (i != null) {
-			i = i.trim().toLowerCase();
-			if (i.equals("quiet")) {
-				localInformative = QUIET;
-			} else if (i.equals("verbose")) {
-				localInformative = VERBOSE;
-			} else if (i.equals("informative")) {
-				localInformative = INFORMATIVE;
-			}
-		} else {
-			informative = informative.trim().toLowerCase();
-			if (informative.equals("quiet")) {
-				localInformative = QUIET;
-			} else if (informative.equals("informative")) {
-				localInformative = INFORMATIVE;
-			} else if (informative.equals("verbose")) {
-				localInformative = VERBOSE;
-			}
-		}
-		arg.add(localInformative);
 		// the template file
+		// TODO: optimize here, check the tag <file> first.
 		String file = (String) m.get("file");
-		//File f = new File(file);
-		arg.add(file);
-		// output
-		String[] argv = arg.toArray(new String[0]);
-		return argv;
+		if (file == null) {
+			getLog().warn("Configuration fail, cannot find the tag <file>");
+			return null;
+		} else {
+			if (isFileNameValid(file)) {
+				// option "-t language"
+				String l = (String) m.get("language");
+				String localLanguage = (isOptionValid(l)) ? l.trim() : language;
+				arg.add("-t");
+				arg.add(localLanguage);
+				// option "-d directory" // TODO: check validation directory
+				String d = (String) m.get("directory");
+				String localDirectory = (isOptionValid(d)) ? d.trim() : directory;
+				arg.add("-d");
+				arg.add(localDirectory);
+				// option "-p packagesname"
+				String p = (String) m.get("packagename");
+				if (p != null) {
+					if (isPackageNameValid(p)) {
+						String localPackagename = p.trim();
+						arg.add("-p");
+						arg.add(localPackagename);
+					} else {
+						throw new RuntimeException("package name not valid:" + p.trim());
+					}
+				} else {
+					if (isPackageNameValid(packagename)) {
+						String localPackagename = packagename.trim();
+						arg.add("-p");
+						arg.add(localPackagename);
+					} else {
+						throw new RuntimeException("package name not valid:" + packagename.trim());
+					}
+				}
+				// option "--generate-code" or "--no-code"
+				String localGenerateCode = generateCode ? GEN_CODE : NO_CODE;
+				String g = (String) m.get("generateCode");
+				if (g != null) {
+					g = g.trim().toLowerCase();
+					localGenerateCode = (g.equals("true") || g.equals("generate-code")) ? GEN_CODE : NO_CODE;
+				}
+				arg.add(localGenerateCode);
+				// option "--strict" or "--lenient"
+				String localStrict = strict ? STRICT : LENIENT;
+				String s = (String) m.get("strict");
+				if (s != null) {
+					s = s.trim().toLowerCase();
+					localStrict = (s.equals("true") || s.equals("strict")) ? STRICT : LENIENT;
+				}
+				arg.add(localStrict);
+				// option "--quiet" or "--informative" or "--verbose"
+				String localInformative = INFORMATIVE;
+				String i = (String) m.get("informative");
+				if (i != null) {
+					i = i.trim().toLowerCase();
+					if (i.equals("quiet")) {
+						localInformative = QUIET;
+					} else if (i.equals("verbose")) {
+						localInformative = VERBOSE;
+					} else if (i.equals("informative")) {
+						localInformative = INFORMATIVE;
+					}
+				} else {
+					informative = informative.trim().toLowerCase();
+					if (informative.equals("quiet")) {
+						localInformative = QUIET;
+					} else if (informative.equals("informative")) {
+						localInformative = INFORMATIVE;
+					} else if (informative.equals("verbose")) {
+						localInformative = VERBOSE;
+					}
+				}
+				arg.add(localInformative);
+				arg.add(file.trim());
+			} else {
+				getLog().warn("Configuration fail, file name: " + file + " invalid");
+				return null;
+			}
+		}
+		// return the result
+		//String[] argv = arg.toArray(new String[0]);
+		return arg;
 	}
 
 	private String constructOutDir() {
@@ -201,27 +203,34 @@ public class ObjectMacroCaller extends AbstractMojo {
 		return directory;
 	}
 
-	boolean isOptionValid(String option){
-		if (option==null){
+	boolean isOptionValid(String option) {
+		if (option == null) {
 			return false;
 		}
-		if (option.trim().length()==0){
+		if (option.trim().length() == 0) {
 			return false;
 		}
 		return true;
 	}
-	boolean isPackageNameValue(String pn){
-		if (!isOptionValid(pn)){
+
+	boolean isPackageNameValid(String pn) {
+		if (!isOptionValid(pn)) {
 			return false;
 		}
-		if (pn.trim().startsWith(".")){
+		if (pn.trim().startsWith(".")) {
 			return false;
 		}
-		for (String n:packageNameNoGo){
-			if (pn.contains(n)){
+		for (String n : packageNameNoGo) {
+			if (pn.contains(n)) {
 				return false;
 			}
 		}
+		return true;
+	}
+
+	// TODO: make more restrict here
+	private boolean isFileNameValid(String file) {
+		assert file != null;
 		return true;
 	}
 }
