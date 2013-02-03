f3c4d17c6f318dfb3715e73ef195bcd24821ed17
verylazyboy <>
2013-02-03 23:12:49 +0100

    use java7javafx to show graph

---
+++ plugin-user-test/src/main/java/de/htwds/pluginusertest/TemplateUser.java
@@ -0,0 +1,13 @@
+
+package de.htwds.pluginusertest;
+import de.htwsaarland.sql.imp.template.MNotSoSimple;
+/**
+ *
+ * @author phucluoi
+ * @version Feb 3, 2013
+ */
+public class TemplateUser {
+	public void useTemplate(String dummy){
+		MNotSoSimple s = new MNotSoSimple(dummy);
+	}
+}
