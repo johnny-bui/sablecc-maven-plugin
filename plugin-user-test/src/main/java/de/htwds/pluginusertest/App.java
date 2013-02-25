36995da69c6e3f09457eefe24c6c0bc67a61ae32
verylazyboy <phucluoi@gmail.com>
2013-02-25 01:25:11 +0100

    modified:   README.md

--- plugin-user-test/src/main/java/de/htwds/pluginusertest/App.java
+++ plugin-user-test/src/main/java/de/htwds/pluginusertest/App.java
@@ -1,9 +1,5 @@
 package de.htwds.pluginusertest;
 
-import de.htwsaarland.sql.imp.template.MCreateTable;
-
-
-
 
 
 /**
@@ -14,6 +10,5 @@ public class App
 {
     public static void main( String[] args )
     {
-		MCreateTable tab = new MCreateTable("hallo");
     }
 }
